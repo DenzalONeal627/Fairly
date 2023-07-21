e9215a8d4aea3a9174c12138c3e15cd4e0bf8eef
DenzalONeal627 <125705014+DenzalONeal627@users.noreply.github.com>
2023-07-20 23:40:44 -0700

    .NET

---
+++ EduReqRequests/EduReqRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.EduReqRequests
+{
+    public class EduReqRequest
+    {
+        public int JobId { get; set; }
+        public int EducationRequirementId { get; set; }
+    }
+}
---
+++ SurveysInstances/SurveyInstance.cs
@@ -0,0 +1,17 @@
+﻿using Sabio.Models.Domain.SurveyAnswers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.SurveysInstances
+{
+    public class SurveyInstance : BaseSurveyInstance
+    {
+        public string SurveyName { get; set; }
+        public LookUp SurveyStatus { get; set; }
+        public List<InstanceSurveyQuestion> Questions { get; set; }
+    }
+
+}
